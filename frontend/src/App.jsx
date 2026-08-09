f1e70e2c40e0020ba3633460791f3b91d4d9a6b2
Chaudharyjayant <chaudharyjayant980@gmail.com>
2026-08-10 01:34:16 +0530

    Set up the frontend

---
+++ frontend/src/App.jsx
@@ -0,0 +1,9 @@
+const App = () => {
+  return (
+    <div className="text-3xl font-bold text-center mt-10 text-blue-600">
+      Gym Management system is getting ready.
+    </div>
+  )
+}
+
+export default App;
